be0e8948896dd97ec1b1c6b3c9c416f5e16e0cd4
dlwoqor0820 <77786424+dlwoqor0820@users.noreply.github.com>
2021-07-12 03:52:46 +0900

    Update background.js

--- js/background.js
+++ js/background.js
@@ -1,9 +1,9 @@
 const images = [
-    "팔조령.jpeg",
-    "아양교.jpg",
-    "욱수지.jpg",
-    "비로봉.jpg",
-    "마르텐사이트.png"
+    "paljo.jpeg",
+    "ayanggyo.jpg",
+    "uksu.jpg",
+    "birobong.jpg",
+    "martensite.png"
 ];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
